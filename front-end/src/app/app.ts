5b82e983d3fb0ac07532f191d368ca11b923f1bc
Matheus Biesek <md.biesek@gmail.com>
2025-09-16 11:59:37 -0300

    feat(components): adiciona navbar e footer
      - Implementa navbar com gradiente azul/roxo e navegação sticky
      - Adiciona footer com gradiente escuro e seções informativas
      - Integra componentes no AppComponent com imports necessários
      - Atualiza app.html para incluir navbar e footer
      - Refatora app.scss removendo estilos antigos e centralizando conteúdo

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
